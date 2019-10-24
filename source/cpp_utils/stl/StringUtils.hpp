eb571d1f4d0810352f134d84ce9707f3be4d81f5
Vladas Zakrevskis <146100@gmail.com>
2019-10-24 14:09:34 +0300

    regex mathes, ttrip

--- source/cpp_utils/stl/StringUtils.hpp
+++ source/cpp_utils/stl/StringUtils.hpp
@@ -8,7 +8,9 @@
 
 #pragma once
 
+#include <regex>
 #include <string>
+#include <vector>
 #include <sstream>
 #include <iomanip>
 
@@ -47,6 +49,55 @@ static std::string from_container(const T& container) {
     result.pop_back();
     result.pop_back();
     return result;
+}
+
+static std::vector<std::string> find_regexpr_matches(const std::string& string, const std::string& query) {
+
+	std::vector<std::string> result;
+	std::regex regex(query);
+	std::smatch match;
+
+	std::string::const_iterator searchStart(string.cbegin());
+	while (regex_search(searchStart, string.cend(), match, regex)) {
+		result.push_back(match[0]);
+		searchStart = match.suffix().first;
+	}
+
+	return result;
+	
+}
+
+static std::string find_regexpr_match(const std::string& string, const std::string& query) {
+	return find_regexpr_matches(string, query).front();
+}
+
+static void drop_first(std::string& string) {
+	string.erase(0, 1);
+}
+
+static void drop_last(std::string& string) {
+	string.erase(string.size() - 1);
+}
+
+static void trim(std::string& string) {
+	drop_first(string);
+	drop_last(string);
+}
+
+static std::string& trimmed(std::string& string) {
+	trim(string);
+	return string;
+}
+
+static void replace(const std::string& replace, const std::string& with, std::string& string) {
+	for (size_t position = 0;; position += with.length()) {
+		position = string.find(replace, position);
+		if (position == std::string::npos) {
+			break;
+		}
+		string.erase(position, replace.length());
+		string.insert(position, with);
+	}
 }
 
 }
