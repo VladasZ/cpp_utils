d348068a4236df8623536c62b18ebae4afcb73d9
Vladas <146100@gmail.com>
2020-01-13 16:05:08 +0300

    fixed test build

--- source/cpp_utils_test/main.cpp
+++ source/cpp_utils_test/main.cpp
@@ -1,27 +1,6 @@
 
 #include "Log.hpp"
-#include "JSONMappable.hpp"
-
-class TestClass : public mapping::JSONMappable<TestClass> {
-
-public:
-
-    int a = 10;
-    std::string b = "10";
-
-    static std::string class_name() {
-        return "TestClass";
-    }
-
-    static auto properties() {
-        return std::make_tuple(
-                mapping::Property("a", &TestClass::a),
-                mapping::Property("b", &TestClass::b)
-        );
-    }
-
-};
 
 int main() {
-  Log(TestClass::parse(TestClass().to_json()).to_json());
+
 }
