38648fadf84f69e58ec165f2210215c266bad3f1
Vladas <146100@gmail.com>
2020-01-15 15:51:11 +0300

    Result refactoring

--- source/cpp_utils/Result.hpp
+++ source/cpp_utils/Result.hpp
@@ -8,46 +8,31 @@
 
 #pragma once
 
-#include <string>
-
-#include "JSONMappable.hpp"
+#include "Log.hpp"
 
 namespace cu {
 
     template<class T>
     class Result {
 
-        static_assert(mapping::is_json_mappable<T> || mapping::is_supported<T>);
-
     public:
 
-        bool found;
-		bool not_found;
-        T object;
+        const bool found;
+		const bool not_found;
+        const T object;
 
-        Result() : found(false), not_found(!found) {
-
+        constexpr Result() : found(false), not_found(!found) {
         }
 
-        Result(const T& object) : object(object), found(true), not_found(!found) {
-
+        constexpr Result(const T& object) : object(object), found(true), not_found(!found) {
         }
 
-        operator T() const {
+        constexpr operator T() const {
             if (!found) {
                 throw std::runtime_error(std::string() +
                                          "Conversion of not found result of type: " + typeid(T).name());
             }
             return object;
-        }
-
-        std::string to_string() {
-            if (found) {
-                return std::string() + "\n" +
-                       "Found result: " "\n"
-                       + object.to_json();
-            }
-            return "Not found result";
         }
 
     };
