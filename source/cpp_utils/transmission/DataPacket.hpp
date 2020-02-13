8fe9aa9089675b68e945c60cfe49100ac9cdb86f
Vladas Zakrevskis <146100@gmail.com>
2020-02-13 17:58:51 +0300

    packets id

--- source/cpp_utils/transmission/DataPacket.hpp
+++ source/cpp_utils/transmission/DataPacket.hpp
@@ -10,6 +10,7 @@
 
 #include <array>
 
+#include "MetaHelpers.hpp"
 #include "PacketHeader.hpp"
 
 namespace cu {
@@ -18,5 +19,13 @@ namespace cu {
     struct DataPacket : public PacketHeader<Data>, public Data {
         using Data::Data;
     };
+
+    template <class  > struct __is_data_packet                 : std::false_type { };
+    template <class T> struct __is_data_packet <DataPacket<T>> : std::true_type  { };
+    template <class T> constexpr bool is_data_packet_v = __is_data_packet<remove_all_t<T>>::value;
+
+    struct Data { };
+
+    template <class T> constexpr bool is_data_v = std::is_base_of_v<Data, remove_all_t<T>>;
 
 }
