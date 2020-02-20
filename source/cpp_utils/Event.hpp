f2dbabf7e6e9a9b0f8f12c424264622180acaf38
Vladas <146100@gmail.com>
2020-02-21 00:38:05 +0300

    event active field

--- source/cpp_utils/Event.hpp
+++ source/cpp_utils/Event.hpp
@@ -32,14 +32,16 @@ namespace cu {
         };
 
     private:
+        bool active = false;
         std::vector<Callback> subscribers;
         std::vector<ObjectSubscriber> object_subscribers;
-
     public:
 
         Event() = default;
 
+        [[nodiscard]]
         Callback& subscribe(void* object) {
+            active = true;
             assert(object != nullptr);
             object_subscribers.emplace_back(object, Callback());
             return object_subscribers.back().callback;
@@ -49,20 +51,23 @@ namespace cu {
             cu::array::remove_where(object_subscribers, [&](const ObjectSubscriber& subscriber) {
                 return subscriber.object == object;
             });
+            if (subscribers.empty() && object_subscribers.empty()) {
+                active = false;
+            }
         }
 
         void operator = (Callback action) {
+            active = true;
             subscribers.push_back(action);
         }
 
         void operator()(Params... parameters) const {
+            if (!active) return;
             for (auto subscriber : subscribers) {
                 subscriber(parameters...);
             }
-            if (object_subscribers.size()) {
-                for (auto& subscriber : object_subscribers) {
-                    subscriber.callback(parameters...);
-                }
+            for (auto& subscriber : object_subscribers) {
+                subscriber.callback(parameters...);
             }
         }
     };
