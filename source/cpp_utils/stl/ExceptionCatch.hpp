d976d9d6e1f794bacbb2b8c3170b1fb3fc85c7aa
Vladas Zakrevskis <146100@gmail.com>
2020-09-14 03:27:32 +0300

    cu::what()

--- source/cpp_utils/stl/ExceptionCatch.hpp
+++ source/cpp_utils/stl/ExceptionCatch.hpp
@@ -8,25 +8,30 @@
 
 #pragma once
 
-#ifdef __cpp_exceptions
-
 #include <string>
 
-[[maybe_unused]]
-static inline std::string what() {
-    auto eptr = std::current_exception();
-    if (!eptr) Fatal("There is no exception.");
-    try   { rethrow_exception(eptr); }
-    catch (const std::exception& e) { return e.what(); }
-    catch (const std::string&    e) { return e; }
-    catch (const char*           e) { return e; }
-    catch (...) { return "Unknown exception."; }
-}
+
+namespace cu {
+
+#ifdef __cpp_exceptions
+
+    [[maybe_unused]]
+    static inline std::string what() {
+        auto eptr = std::current_exception();
+        if (!eptr) Fatal("There is no exception.");
+        try   { rethrow_exception(eptr); }
+        catch (const std::exception& e) { return e.what(); }
+        catch (const std::string&    e) { return e; }
+        catch (const char*           e) { return e; }
+        catch (...) { return "Unknown exception."; }
+    }
 
 #else
 
-static std::string what() {
-    return "__cpp_exceptions disabled";
-}
+    static std::string what() {
+        return "__cpp_exceptions disabled";
+    }
 
 #endif
+
+}
