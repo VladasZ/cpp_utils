4cb521f4499fdd0bd776c2301b6fe18a947058ea
Vladas Zakrevskis <146100@gmail.com>
2020-10-08 20:49:40 +0300

    range conversion sane limits

--- source/cpp_utils/dsp/RangeConversion.hpp
+++ source/cpp_utils/dsp/RangeConversion.hpp
@@ -11,50 +11,59 @@
 
 namespace cu {
 
-    class RangeConversion {
+	class RangeConversion {
 
-    protected:
+	protected:
 
-        float _value_range = 1;
-        float _converted_range = 1;
+		float _value_range = 1;
+		float _converted_range = 1;
 
-        float _min = 0;
-        float _max = 1;
+		float _min = 0;
+		float _max = 1;
 
-        float _target_min = 0;
-        float _target_max = 1;
+		float _target_min = 0;
+		float _target_max = 1;
 
-    public:
+		float _min_sane = 0;
+		float _max_sane = 1;
 
-        bool flip = false;
+	public:
 
-        virtual float convert(float value) const {
-            auto normalized_value = (value - _min) / _value_range;
-            auto result = _converted_range * normalized_value + _target_min;
-            if (flip) {
-                result = _converted_range - result;
-            }
-            return result;
-        }
+		bool flip = false;
 
-        float convert_back(float value) const {
-            return value / _target_max;
-        }
+		virtual float convert(float value) const {
+			auto normalized_value = (value - _min) / _value_range;
+			auto result = _converted_range * normalized_value + _target_min;
+			if (flip) {
+				result = _converted_range - result;
+			}
 
-        float min() const { return _min; }
-        float max() const { return _max; }
+			if (result < _min_sane) return _min_sane;
+			if (result > _max_sane) return _max_sane;
+									return result;
+		}
 
-        void set_min(float min) { _min = min; _update_range(); }
-        void set_max(float max) { _max = max; _update_range(); }
+		float convert_back(float value) const {
+			return value / _target_max;
+		}
 
-        void set_target_min(float min) { _target_min = min; _update_converted_range(); }
-        void set_target_max(float max) { _target_max = max; _update_converted_range(); }
+		float min() const { return _min; }
+		float max() const { return _max; }
 
-    protected:
+		void set_min(float min) { _min = min; _update_range(); }
+		void set_max(float max) { _max = max; _update_range(); }
 
-        virtual void _update_range() { _value_range = _max - _min; }
-        void _update_converted_range() { _converted_range = _target_max - _target_min; }
+		void set_target_min(float min) { _target_min = min; _min_sane = std::min(min, _min_sane); _update_converted_range(); }
+		void set_target_max(float max) { _target_max = max; _max_sane = std::max(max, _max_sane); _update_converted_range(); }
 
-    };
+		void set_min_sane(float min) { _min_sane = min; }
+		void set_max_sane(float max) { _max_sane = max; }
+
+	protected:
+
+		virtual void _update_range() { _value_range = _max - _min; }
+		void _update_converted_range() { _converted_range = _target_max - _target_min; }
+
+	};
 
 }
