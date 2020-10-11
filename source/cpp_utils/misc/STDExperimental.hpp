564044df28106ef44be5a28174655ec2656ff5d4
Vladas Zakrevskis <146100@gmail.com>
2020-10-11 17:42:32 +0300

    fixed std experimental

--- source/cpp_utils/misc/STDExperimental.hpp
+++ source/cpp_utils/misc/STDExperimental.hpp
@@ -8,12 +8,13 @@
 
 #pragma once
 
-#ifdef EXPERIMENTAL_OPTIONAL
+
+#if __has_include(<optional>)
+#include <optional>
+#else
 #include <experimental/optional>
 namespace std {
     template<class T>
     using optional = std::experimental::optional<T>;
 }
-#else
-#include <optional>
 #endif
